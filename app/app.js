ebdf584caee515767f50dacff578c96917c607fe
Dan Moneypenny <catullus1973@gmail.com>
2020-07-06 12:51:25 -0700

    add hbs utility

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const createError = require('http-errors');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const flash = require('connect-flash');
+const Handlebars = require('handlebars');
 const handleError = require('./middleware/handleError');
 const helmet = require('helmet');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
 const hpp = require('hpp');
+const methodOverride = require('method-override');
 const mongoSanitize = require('express-mongo-sanitize');
 const path = require('path');
 const rateLimit = require('express-rate-limit');
@@ -42,8 +47,10 @@ app.use(limiter);
 
  app.use(express.json());
  app.use(express.urlencoded({ extended: false }));
- //app.use(express.static(path.join(__dirname, 'public')));
+ app.use(express.static(path.join(__dirname, 'public')));
  app.use(cookieParser());
+ app.use(flash());
+ app.use(methodOverride('_method'));
  app.use(session({
   secret: SESSION_SECRET,
   resave: true,
@@ -56,6 +63,13 @@ app.use(limiter);
 /** 
  * @desc GLOBAL VARIABLES
  */
+
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.user = req.user || null;
+  next();
+}); 
 
 if (ISDEV) {
   const logger = require('morgan');
